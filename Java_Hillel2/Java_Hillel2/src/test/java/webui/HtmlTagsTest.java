2933d50407fe7e77599e09d9983f4be94086aa33
YaroslavLyutyy <yaroslav.lyutyy@gmail.com>
2022-09-16 17:50:32 +0300

    Init commit

--- Java_Hillel2/Java_Hillel2/src/test/java/webui/HtmlTagsTest.java
+++ Java_Hillel2/Java_Hillel2/src/test/java/webui/HtmlTagsTest.java
@@ -7,37 +7,63 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import webui.exceprions.CustomAssertionError;
 
 public class HtmlTagsTest {
-//    1. All JS on page are imported as files
+    //    1. All JS on page are imported as files
 //    2. Check 1 JS.
 //    3. All CSS are imported as files
 //    4. Check 1 CSS
 //    5. Check all IDs are unique
     private WebDriver driver;
+
     @Test
-    public void checkAllJsAreFiles () {
+    public void checkAllJsAreFiles() {
         int scriptsSize = driver.findElements(By.cssSelector("script:not([src])")).size();
         try {
             Assert.assertEquals(scriptsSize, 0);
         } catch (AssertionError e) {
-            System.out.println("Test Failed .... ");
+            throw new CustomAssertionError(e.getMessage());
         }
-        Assert.assertEquals(scriptsSize, 1);
+
     }
+
     @Test
-    public void checkSingleJs() {
+    public void checkSingleJS() {
 
         int allScripts = driver.findElements(By.cssSelector("script")).size();
+        Assert.assertEquals(allScripts, 1);
     }
+
+    @Test
+    public void checkAllCSSAreFiles() {
+
+        int stylesSize = driver.findElements(By.cssSelector("style")).size();
+        Assert.assertEquals(stylesSize, 0);
+    }
+
+    @Test
+    public void checkSingleCSS() {
+
+        int allStyles = driver.findElements(By.cssSelector("link[rel='stylesheet']")).size();
+        Assert.assertEquals(allStyles, 1);
+    }
+
+    @Test
+    public void checkUniqueIDs() {
+        driver.findElements(By.cssSelector("*[id]"));
+
+    }
+
     @BeforeClass
-    public void setup (){
+    public void setup() {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         driver = new ChromeDriver();
-        driver.get("https://rapidapi.com/");
+        driver.get("https://stackoverflow.com");
     }
+
     @AfterClass
-    public void teardown(){
+    public void teardown() {
         driver.close();
     }
 }
